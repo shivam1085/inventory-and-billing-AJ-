22764237b3c8f67f93cd6dcd47718b50ed0e1854
Shivam Malviya <shivam.malviya248@gmail.com>
2025-11-09 14:52:35 +0530

    Configure PostgreSQL database for persistent data storage

--- autoparts/settings.py
+++ autoparts/settings.py
@@ -98,6 +98,13 @@ DATABASES = {
     }
 }
 
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
